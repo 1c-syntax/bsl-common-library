5fba4a8d302703f794eba5faa7286d706388dcfd
theshadowco <maximovvalery@gmail.com>
2022-02-20 07:28:29 +0300

    update license

--- src/test/java/com/github/_1c_syntax/bsl/support/CompatibilityModeTest.java
+++ src/test/java/com/github/_1c_syntax/bsl/support/CompatibilityModeTest.java
@@ -1,7 +1,7 @@
 /*
  * This file is a part of BSL Common library.
  *
- * Copyright © 2021 - 2022
+ * Copyright (c) 2021 - 2022
  * Tymko Oleg <olegtymko@yandex.ru>, Maximov Valery <maximovvalery@gmail.com> and contributors
  *
  * SPDX-License-Identifier: LGPL-3.0-or-later
